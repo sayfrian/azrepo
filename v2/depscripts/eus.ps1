68b40af294b3e01218e7026f292ab563783be0d7
sayfuladrian <sayful.adrian@gmail.com>
2022-11-02 16:29:14 +0700

    update scripts as needed

--- v2/depscripts/eus.ps1
+++ v2/depscripts/eus.ps1
@@ -201,6 +201,30 @@ else{
 }
 
 Write-Output `n "======================================================================================" 
+Write-Output    "==============================  Creating VPN Network  ================================" 
+Write-Output    "======================================================================================" `n
+
+$vpn = 'eus-vpn'
+
+$check = Get-AzPublicIpAddress -Name $vpn -ErrorAction SilentlyContinue
+
+if($check -eq $null){
+
+	New-AzResourceGroupDeployment `
+	  -Name remoteTemplateDeployment `
+	  -ResourceGroupName $rg `
+	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/vpn-act-nobgp.json" `
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/$vpn.json"
+	  
+}
+
+else{
+
+    Write-Host " $vpn already exist"
+
+}
+
+Write-Output `n "======================================================================================" 
 Write-Output    "==============================  Creating VNetIntface  ================================" 
 Write-Output    "======================================================================================" `n
 
