f3b9cd4761c830e99ac746db8ac84b3b547ca84b
sayfuladrian <sayful.adrian@gmail.com>
2022-11-02 15:54:30 +0700

    update scripts as needed

--- v2/depscripts/eus.ps1
+++ v2/depscripts/eus.ps1
@@ -156,6 +156,50 @@ else{
 
 }
 
+Write-Output    "===================================  On progress  ===================================="
+
+$pip = 'eus-pipvpna'
+
+$check = Get-AzPublicIpAddress -Name $pip -ErrorAction SilentlyContinue
+
+if($check -eq $null){
+
+	New-AzResourceGroupDeployment `
+	  -Name remoteTemplateDeployment `
+	  -ResourceGroupName $rg `
+	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/pipBsc.json" `
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/" . $pip . ".json"
+	  
+}
+
+else{
+
+    Write-Host " $pip already exist"
+
+}
+
+Write-Output    "===================================  On progress  ===================================="
+
+$pip = 'eus-pipvpnb'
+
+$check = Get-AzPublicIpAddress -Name $pip -ErrorAction SilentlyContinue
+
+if($check -eq $null){
+
+	New-AzResourceGroupDeployment `
+	  -Name remoteTemplateDeployment `
+	  -ResourceGroupName $rg `
+	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/pipBsc.json" `
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/eus-pipvpnb.json"
+	  
+}
+
+else{
+
+    Write-Host " $pip already exist"
+
+}
+
 Write-Output `n "======================================================================================" 
 Write-Output    "==============================  Creating VNetIntface  ================================" 
 Write-Output    "======================================================================================" `n
--- v2/depscripts/ind.ps1
+++ v2/depscripts/ind.ps1
@@ -1,8 +1,4 @@
-Write-Output `n "======================================================================================" 
-Write-Output    "===============================  Creating PublicIPAdd ================================" 
-Write-Output    "======================================================================================" `n 
-
-$pip = 'eus-pipad19a'
+$pip = 'eus-pipvpna'
 
 $check = Get-AzPublicIpAddress -Name $pip -ErrorAction SilentlyContinue
 
@@ -12,51 +8,7 @@ if($check -eq $null){
 	  -Name remoteTemplateDeployment `
 	  -ResourceGroupName $rg `
 	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/pipBsc.json" `
-	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/eus-pipad19a.json"
-	  
-}
-
-else{
-
-    Write-Host " $pip already exist"
-
-}
-
-Write-Output    "===================================  On progress  ===================================="
-
-$pip = 'eus-pipws10a'
-
-$check = Get-AzPublicIpAddress -Name $pip -ErrorAction SilentlyContinue
-
-if($check -eq $null){
-
-	New-AzResourceGroupDeployment `
-	  -Name remoteTemplateDeployment `
-	  -ResourceGroupName $rg `
-	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/pipBsc.json" `
-	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/eus-pipws10a.json"
-	  
-}
-
-else{
-
-    Write-Host " $pip already exist"
-
-}
-
-Write-Output    "===================================  On progress  ===================================="
-
-$pip = 'eus-pipws10b'
-
-$check = Get-AzPublicIpAddress -Name $pip -ErrorAction SilentlyContinue
-
-if($check -eq $null){
-
-	New-AzResourceGroupDeployment `
-	  -Name remoteTemplateDeployment `
-	  -ResourceGroupName $rg `
-	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/pipBsc.json" `
-	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/eus-pipws10b.json"
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/" . $pip . ".json"
 	  
 }
 
