3b786c621c9f34d8d2d06ce3d67bdc32a317f170
sayfuladrian <sayful.adrian@gmail.com>
2022-11-03 09:15:32 +0700

    update scripts as needed

--- v2/depscripts/ind.ps1
+++ v2/depscripts/ind.ps1
@@ -1,19 +1,19 @@
-$vm = 'eus-vmad19a'
+$vnic = 'eus-vnicad19a'
 
-$check = Get-AzVM -Name $vm -ErrorAction SilentlyContinue
+$check = Get-AzNetworkInterface -Name $vnic -ErrorAction SilentlyContinue
 
 if($check -eq $null){
 
 	New-AzResourceGroupDeployment `
 	  -Name remoteTemplateDeployment `
 	  -ResourceGroupName $rg `
-	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/vm-nopip-nodisk-ps.json" `
-	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/eus-vmad19a.json"
+	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/vnic-ip4.json" `
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/$vnic.json"
 	  
 }
 
 else{
 
-    Write-Host "$vm already exist"
+    Write-Host "$vnic already exist"
 
 }
