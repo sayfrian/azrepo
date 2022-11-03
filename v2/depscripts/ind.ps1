c51238a1620fe650ff12d70efec56672de890dfa
sayfuladrian <sayful.adrian@gmail.com>
2022-11-03 09:36:05 +0700

    update scripts as needed

--- v2/depscripts/ind.ps1
+++ v2/depscripts/ind.ps1
@@ -17,3 +17,47 @@ else{
     Write-Host "$vnic already exist"
 
 }
+
+Write-Output    "===================================  On progress  ====================================" 
+
+$vnic = 'eus-vnicws10a'
+
+$check = Get-AzNetworkInterface -Name $vnic -ErrorAction SilentlyContinue
+
+if($check -eq $null){
+
+	New-AzResourceGroupDeployment `
+	  -Name remoteTemplateDeployment `
+	  -ResourceGroupName $rg `
+	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/vnic-ip4.json" `
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/$vnic.json"
+	  
+}
+
+else{
+
+    Write-Host "$vnic already exist"
+
+}
+
+Write-Output    "===================================  On progress  ====================================" 
+
+$vnic = 'eus-vnicws10b'
+
+$check = Get-AzNetworkInterface -Name $vnic -ErrorAction SilentlyContinue
+
+if($check -eq $null){
+
+	New-AzResourceGroupDeployment `
+	  -Name remoteTemplateDeployment `
+	  -ResourceGroupName $rg `
+	  -TemplateUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/templates/vnic-ip4.json" `
+	  -TemplateParameterUri "https://raw.githubusercontent.com/sayfuladrian/azrepo/main/v2/parameters/$vnic.json"
+	  
+}
+
+else{
+
+    Write-Host "$vnic already exist"
+
+}
